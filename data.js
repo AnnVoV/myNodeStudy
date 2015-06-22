0bd68ae733dab8c2be28228564a5a547f3729c4d
ann <hzzhenganran@corp.netease.com>
2015-06-22 16:50:29 +0800

    express 路由中间件

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 var model = {
-  touristPlaces:[]
+  touristPlaces:[],
+  detailList:{}
 };
 
 var controller = {
@@ -18,6 +19,11 @@ var controller = {
       {place:'日本',price:'10000',status:'sunny'},
       {place:'美国',price:'15000',status:'cloudy'}
     ];
+    model.detailList = {
+      '台湾':{title:'台湾',subtitle:'taiwan',status:'cloudy',intro:'I will go there next summer!Must Go!'},
+      '日本':{title:'日本',subtitle:'japan',status:'sunny',intro:'I want to go to this clean place!'},
+      '美国':{title:'美国',subtitle:'usa',status:'rainy',intro:'Forever No.1 in my heart!Love Love Love!'}
+    };
   }
 };
 
@@ -31,6 +37,10 @@ main();
 //返回旅游列表数据
 exports.getPlaceList = function(){
   var arr = controller.getPlaceList();
-  console.log(arr);
   return arr;
+};
+
+//返回具体的景点信息列表
+exports.getDetailList = function(){
+  return model.detailList;
 };
