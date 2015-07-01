91b68ba5253ba0b34960d3ce3b5b25ba6ce7871b
AnnVoV <hzzhenganran@corp.netease.com>
2015-07-01 15:33:22 +0800

    generator内部遍历器学习

--- js/bootstrap.js
+++
@@ -1,8 +0,0 @@
-var System = require('es6-module-loader').System;
-
-System.import('some-module').then(function(m) {
-    //index.run(__dirname);
-    console.log(m.p);
-}).catch(function(err){
-    //console.log('err', err);
-});
--- js/point.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-import math from './index';
-
-console.log(math.add(1,2));
-console.log(math.minus(2,1));
