1ecb9ba70304de187277d57dc26e0269f48e692f
AnnVoV <hzzhenganran@corp.netease.com>
2015-07-08 21:27:44 +0800

    Koa 中间件的学习2

---
+++ app_generator.js
@@ -0,0 +1,43 @@
+var koa = require('koa'),
+    app = koa();
+
+var testMethod = function(data){
+  //koa 中app.use的参数必须为next
+  return function* (next){  
+    console.log('这是执行的第一个中间件');
+    data++;
+    //我之前写的document.body
+    //后台程序拿不到前端页面的document.body
+    //那为什么写this就可以拿到
+    this.body += "Hello World!"+data;
+    //如果带有next 参数则默认去掉用下一个中间件
+    yield next;
+  };
+};
+
+app.use(testMethod(1));
+
+app.use(function* (next){
+  console.log('这是下一个中间件');
+  console.log('-------- end --------');
+  //注意这里不要写成了 yield next()
+  yield next;
+});
+
+app.use(function* (){
+  this.body+='\n header \n';
+  //如果这里不绑定this 会出错，但并不理解原因
+  yield theLastGen.call(this);
+  this.body+=' footer\n';
+});
+
+function* theLastGen(){
+  this.body+=' body\n'
+  console.log('--------- last 中间件 -------');
+}
+
+app.listen(8088);
+
+//注意：在Koa框架中，我们要使用app.use 默认的参数必须为next
+//所以如果我们使用了自定义的函数我们必须自己返回一个generator函数
+//要把它包裹起来
