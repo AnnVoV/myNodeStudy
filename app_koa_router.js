c07bc0f463d799c4fa9c7a8d5e5684e48407bebd
AnnVoV <hzzhenganran@corp.netease.com>
2015-07-11 17:22:56 +0800

    koa-router中间件学习

--- app_koa_router.js
+++ app_koa_router.js
@@ -4,6 +4,19 @@ var router = new Router();
 var pre_router = new Router({
   prefix:'/list'
 });
+var users = ['ann0','ann1','ann2',
+  {
+    name:'ann3',getFriends:function* (){
+      return [
+        'amber',
+        'cloud huang',
+        'linkey',
+        'meimei',
+        'daisy'
+      ];
+    }
+  }
+];//用户数组
 
 pre_router.get('/',function* (next){
   this.body = 'This is list page!';
@@ -20,17 +33,45 @@ router.get('/',function* (next){
   this.body = 'Welcome to homepage!';
 });
 
-router.get('/users',function* (){
+router
+  .param('user', function *(id, next) {
+    console.log(users[id]);
+    this.user = users[id];
+    if (!this.user) return this.status = 404;
+      yield next;
+    }
+  )
+  .get('/users/:user', function *(next) {
+    this.body = this.user;
+  })
+  .get('/users/:userId/friends', function *(next) {
+    console.log('xxx:',this.user);
+    this.body = yield this.user.getFriends();
+  });
+
+router.get('/user',function* (){
   this.body = 'Welcome to user page!';
 });
 
-router.get('/users/:id',function* (next){
-  if(this.path.match(/\/users\/*/) == null){
+router.get('/user/:id',function* (next){
+  if(this.path.match(/\/user\/*/) == null){
     return yield next;
   }
   this.body = 'Welcome to user page! param:id->'+this.params.id;
   console.log('id:',this.params.id);
 });
+
+router.get('/:category/:id',function* (next){
+  console.log(this.params);
+  this.body  = this.params;
+});
+
+//定向跳转页面 redirect 方法
+//当我们请求/login页面时，会跳转到sign-in页面
+router.redirect('/login','sign-in')
+  .get('/sign-in',function* (){
+      this.body = 'Sign in to login';
+  });
 
 router.get('*',function* (){
   this.body = '404 not found';
