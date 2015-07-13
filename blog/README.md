9d83ecf9f4f66c744ccd67c3d9f7572a734d33d8
AnnVoV <hzzhenganran@corp.netease.com>
2015-07-13 12:04:42 +0800

    koa,xtpl,cookie,route

--- blog/README.md
+++ blog/README.md
@@ -1,5 +1,6 @@
 ####xtpl与koa框架的结合使用   
-[xtpl的相关参考文章:](https://www.npmjs.com/package/xtpl)
+[xtpl的相关参考文章:](https://www.npmjs.com/package/xtpl)  
+[koa 的可以参考文章:](http://www.atatech.org/articles/30923)
 ####index.js  
 ````
   var app = require('./lib/koa')(require('koa')(),{
@@ -13,4 +14,8 @@
   app.listen(8089);
 
 ````
-####使用koa-bodyparser模块
+####使用koa-bodyparser模块 处理与post相关数据的内容  
+[koa的body-parser模块](https://github.com/koajs/body-parser)  
+
+####要注意yield 的使用
+
--- blog/index.js
+++ blog/index.js
@@ -3,28 +3,50 @@ var app = require('./lib/koa')(require('koa')(),{
 }),
     router = require('koa-router')(),
     //使用koa-bodyparser 组件处理post来的数据
-    bodyParser = require('koa-bodyparser');
-
-var global = {};
+    bodyParser = require('koa-bodyparser'),
+    global = {};
 
 router.get('/',function* (){
-  yield this.render('login',{});
+  if(this.cookies.get('name') && this.cookies.get('pass')){
+    //如果存储过name于pass的相关cookie信息
+    console.log('enter into cookie...');
+    var name = this.cookies.get('name'),
+        pass = this.cookies.get('pass'),
+        cookieData = {name:name,pass:pass};
+    //如果有cookie直接把数据绘制到前端
+    yield this.render('login',{cookieData:cookieData});
+  }else{
+    yield this.render('login',{});
+  }
 });
-//上述代码等价于
-//app.use(function*(){
-    //yield this.render('login',{});
-//});
+
+router.get('/test',function* (next){
+  console.log('enter test');
+  yield this.render('test',
+    { name: "foo", 
+      array: [
+        {name: "bar"},
+        {name: "ann"},
+        {name: "amber"}
+      ]
+    }
+  );
+})
 
 router.post('/loginHandle',function* (next){
   //使用了koa-bodyparser以后，解析后的数据会存放到this.request.body里面
   global.jsondata = this.request.body;
   yield dataValidate(next);
   //如果验证正确跳转到sign_in页面
+  if(!this.cookies.get('name') && !this.cookies.get('pass')){
+    this.cookies.set('name',global.name);
+    this.cookies.set('pass',global.pass);
+  }
   this.redirect('/sign_in');
 });
 
 router.get('/sign_in',function* (next){
-  this.body = ' Hello ' + global.jsondata.name +', welcome back! ';
+  yield this.render('signin',{});
 });
 
 router.get('*',function* (){
@@ -35,7 +57,8 @@ router.get('*',function* (){
 function* dataValidate(next){
   var name = global.jsondata.name,
       pass = global.jsondata.pass;
-  console.log('name:',name,' pass:',pass);
+  global.name = name;
+  global.pass = pass;
   if( name != 'ann' || pass != '123456' ){
     //如果name和pass不符合我们的判断条件
     console.log('wrong');
